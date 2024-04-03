82882161df0b33ce98f26e688f2cdc21c3e572b0
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-03 12:48:37 +0000

    config add

---
+++ .devcontainer/js_Basic/varibles.js
@@ -0,0 +1,22 @@
+const employeeId = 7
+let employeeName = "Tech_Zen"
+var employeeDirection = "Surat"
+employeeRegion = "India"
+
+
+//Changes in strings
+employeeDirection = "Kamrej"
+employeeName = "ZenTech"
+employeeRegion = "Indian"
+
+// Undefined value 
+let employeeCity;
+
+console.table([employeeId, employeeName, employeeDirection, employeeCity]);
+
+4
+
+/* 
+Prefer not to use var bcz Funtional Scope Error and  it is hard to use var
+*/
+
