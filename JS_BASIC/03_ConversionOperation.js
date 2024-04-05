d0bd976f1c6278a2d57db9596cd8c082afd86c28
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-05 11:39:36 +0000

    config

--- JS_BASIC/03_ConversionOperation.js
+++ JS_BASIC/03_ConversionOperation.js
@@ -1,6 +1,20 @@
 console.log("Zenil");
 
-let Iq = 110
+let Iq = "110Zen"
+let Ability = null 
+let Street = undefined
+let condition = true 
+let name = "Zen"
+
+
 
 console.log(typeof Iq)
 console.log(typeof (Iq));
+
+// convert string into number 
+let valueInNumber = Number(Ability)
+console.log(typeof valueInNumber)
+// find current what value is of number 
+console.log(valueInNumber);
+
+
