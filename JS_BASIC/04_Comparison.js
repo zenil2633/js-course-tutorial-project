6e02487627a3e74ee9ded34718e9294f40ae214c
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-09 12:26:12 +0000

    Comparison

---
+++ JS_BASIC/04_Comparison.js
@@ -0,0 +1,34 @@
+console.log("Comparison Operators ");
+
+// === Basic Comparison Operators ===
+console.log(7 < 5);
+console.log(7 <= 5);
+console.log(7 > 5);
+console.log(7 >= 6);
+console.log(7 == 3);
+console.log(7 != 4);
+  
+console.log("Tricky Comparison & Avoid");
+
+console.log(undefined = 1);
+console.log( undefined == 1);
+console.log(undefined <= 1);
+
+console.log(null > 1);
+console.log(null == 1);
+console.log(null <= 1); // The Reason is behind this true value is equally compared value ==  & <=
+
+
+//  Symbol of Strict Check "==="
+
+console.log("Strict Check");
+
+console.log(2 === 2);
+console.log("2" === 2);
+console.log( "Zen" === "Dungrani");
+
+
+
+
+
+
