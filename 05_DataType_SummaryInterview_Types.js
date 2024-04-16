e3c3f1ea3dc170c8d2d2f596df85e569f9bbd5bd
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-16 12:02:22 +0000

    05_DataType_SummaryInterview_Types

---
+++ 05_DataType_SummaryInterview_Types.js
@@ -0,0 +1,26 @@
+// DataTypes is divided into Two different groups 
+
+//  A :- Primitive 
+//  7 Types : - String, Boolean, BigInt, Number, Null, Undefined, Symbol 
+// Symbol is occationally Used Just Like Idenfied D.T
+
+const Surname =  ("Dungrani")
+const IsLoggedIn = true
+const BigNumber = 77777444442343942n
+const IdNumber = 7
+const TempDown = null
+let Iq;
+   const Id = Symbol("Zenil")
+   const IdNum = Symbol("777")
+
+   console.log(Id === IdNum);
+   console.log(typeof IdNumber);
+   console.log(Surname);
+   console.log(IsLoggedIn);
+   console.log(TempDown);
+   console.log(typeof BigNumber);
+   
+
+//  B :- Reference [Non-Primitive]
+//  Array, Objects, Function
+
