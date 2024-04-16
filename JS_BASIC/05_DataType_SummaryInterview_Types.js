a69e087365e34bb14e2f3dd06f14c223e4086690
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-16 12:53:53 +0000

    05_DataType_SummaryInterview_Types

--- JS_BASIC/05_DataType_SummaryInterview_Types.js
+++ JS_BASIC/05_DataType_SummaryInterview_Types.js
@@ -48,3 +48,36 @@ console.log(typeof Books);
 console.log(mybooks);
 console.log(typeof mybooks);
 console.log(typeof mySkills);
+
+
+
+
+//  +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+// Memory = Stack & Heap 
+
+// Stack is [Primitive DataType]
+
+//  Stack is also return Only Copy name
+let Mystrength = ("I am better Communicator.")
+
+let anotherStrength = Mystrength
+anotherStrenth = ("I am good at solving the  problem.")
+
+console.log(Mystrength);
+console.log(anotherStrenth);
+
+
+
+//  Heap is [Non - Primitive]
+//  While, Heap is return reference name and changing value of both side
+let myIELTSCORE = {
+    ReadingBand: 7.0,
+    WritingBand: 6.5,
+}
+let myIELTSCOREAll = myIELTSCORE
+// (. = dot is used to change the value and access )
+myIELTSCOREAll.ReadingBand = 7.5     
+console.log(myIELTSCORE);
+console.log(myIELTSCOREAll);
