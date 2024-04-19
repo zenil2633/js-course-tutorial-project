9e8b51a7ac925a187440e061b67326e9d35adcb3
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-04-19 12:03:52 +0000

    08_Date_And_Time

---
+++ JS_BASIC/08_Date_And_Time.js
@@ -0,0 +1,35 @@
+console.log("Dates")
+
+// Basic Function  of date
+let Currentdate = new Date()
+console.log(Currentdate.toDateString());
+console.log(Currentdate.toISOString());
+console.log(Currentdate.toJSON());
+console.log(Currentdate.toLocaleDateString());
+console.log(Currentdate.toLocaleString());
+console.log(Currentdate.toString());
+console.log(Currentdate.toLocaleTimeString());
+console.log(Currentdate.toUTCString());
+console.log(typeof Currentdate);
+
+
+// Example 
+console.log("Examples");
+let myBorndate = new Date(2003, 2, 4)
+let mydate = new Date("02-04-2003")
+let myTimedate = new Date(2003, 2, 4, 5, 4)
+console.log(myBorndate.toString());
+console.log(myBorndate.toLocaleString());
+console.log(myBorndate.toLocaleTimeString());
+
+// TimeStamp 
+console.log("TimeStamp");
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log("As compare with myBorndate");
+console.log(myBorndate.getTime());
+console.log(Math.floor(Date.now()/1000));
+
+
+
