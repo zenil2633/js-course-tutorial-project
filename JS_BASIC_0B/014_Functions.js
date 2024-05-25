6e4acd40cce7b8f09866a36f1a97a682f3ab3f3e
zenil2633 <123535757+zenil2633@users.noreply.github.com>
2024-05-25 11:01:53 +0000

    014_Function

--- JS_BASIC_0B/014_Functions.js
+++ JS_BASIC_0B/014_Functions.js
@@ -48,3 +48,35 @@ function LoginUserMsg(username = "Zenil") {
 }
 // console.log(LoginUserMsg("Zenil"));
 console.log(LoginUserMsg("Tirth"));
+
+
+// Shop cart price total 
+function calculateitems(val1, val2, val3, ...TotalItems) {
+    return TotalItems
+}
+console.log(calculateitems(200, 400, 600, 800, 2000, 4000))
+
+
+// How to add object into function
+const Skills = {
+    technical: "JavaScript Programming",
+    softprice: 2000
+}
+
+function Combineskill(anyskill) {
+    console.log(`My Expertise in ${anyskill.technical} and Cost Will be $${anyskill.softprice} `);
+}
+Combineskill(Skills)
+
+// How to add array into function 
+const myNewArray = [300, 600, 700, 400, 444]
+
+function returnNewArray(getarr) {
+    return getarr[2]
+}
+console.log(returnNewArray(myNewArray));
+console.log(returnNewArray([2020, 2203, 4400, 40404]));  // direct pass value
+
+
+
+
